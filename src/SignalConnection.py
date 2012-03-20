2ebc2c008d8db16a826388415ce4059c3f986503
Jaakko Korkeaniemi <jkorkean@cc.hut.fi>
2012-03-20 12:04:07 +0200

    Fine tune signal server
    Use operation dictionary for codes and operations, initialize
    sequence numbers and session ids.

--- src/SignalConnection.py
+++ src/SignalConnection.py
@@ -1,4 +1,4 @@
-import logging, socket, signal, sys, time
+import logging, socket, signal, sys, time, random
 from threading import Thread
 
 from PacketManager import *
@@ -16,7 +16,7 @@ class SignalServer(Thread):
 	received_packet = None
 
 	# TODO Throw error in case bind fails (Might do it already...)
-	def __init__(self, ip = "127.0.0.1", port = 5500):
+	def __init__(self, ip = "0.0.0.0", port = 5500):
 		Thread.__init__(self)
 
 		# TODO Think trough how the program should exit
@@ -55,11 +55,12 @@ class SignalServer(Thread):
 					found = True
 					self.logger.info("packet belongs to existing connection")
 					break
-			if not found and self.received_packet.otype == 1 :
+			if not found and self.received_packet.otype == OPERATION['HELLO'] and \
+					self.received_packet.ocode == CODE['REQUEST'] :
 				connection = Connection(self, addr[0], addr[1], 
-					2, self.received_packet.txlocalID, # TODO init localsession id
-					self.received_packet.version, 1000) # TODO init seq no
-				connection.hello_recv()
+					self.get_new_session_id(random.randint(0, 65536)), self.received_packet.txlocalID,
+					self.received_packet.version, random.randint(0, 65536))
+				connection.hello_recv(self.received_packet.sequence)
 				self.connection_list.append(connection)
 				self.logger.info("hello packet received, new connection established\
 (local id %d, remote id %d) and HELLO sent" % (connection.local_session_id, connection.remote_session_id))
@@ -75,9 +76,16 @@ class SignalServer(Thread):
 		for destination in destination_list:
 			self.logger.info('connecting to ' + destination[0] + ', ' + destination [1])
 			#self.sock.sendto("daddaa", (destination[0], int(destination[1])) )
-			connection = Connection(self, destination[0], int(destination[1]), 1)
+			connection = Connection(self, destination[0], int(destination[1]), self.get_new_session_id(random.randint(0, 65536)))
 			connection.connect()
 			self.connection_list.append(connection)
+	
+	# Returns an unigue local session_id. Takes a random number from 0 to 65536 as a parameter.
+	def get_new_session_id(self, rand_no):
+		for connection in self.connection_list:
+			if rand_no == connection.local_session_id:
+				return get_new_session_id(random.randint(0, 65536))
+		return rand_no
 
 	def stop(self):
 		self.logger.info("server should stop")
@@ -127,29 +135,36 @@ class Connection:
     		#def create_packet(self, version=1, flags=0, senderID=0, txlocalID=0, txremoteID=0,
                 #     sequence=0, ack=0, otype=0, ocode=0, TLVlist=None, rawdata=None):
 		# Packet manager should be able to build hello packets (i.e. set remote session id)
-		self.packet.create_packet(self.version, 0, self.server.sender_id, self.local_session_id,
-			0, self.seq_no, self.ack_no, 1)  
+		self.packet.create_packet(version=self.version, flags=0, senderID=self.server.sender_id,
+			txlocalID=self.local_session_id, txremoteID=0, sequence=self.seq_no, otype='HELLO',
+			ocode='REQUEST')  
 		self.server.sock.sendto(self.packet.build_packet(), (self.remote_ip, self.remote_port) )
 		self.state = Connection.State.HELLO_SENT
 		# TODO set timers
 
-	def hello_recv(self):
-		self.packet.create_packet(self.version, 0, self.server.sender_id, self.local_session_id,
-			self.remote_session_id, self.seq_no, self.ack_no, 1)  
+		# Needs the seq no of the HELLO packet as a parameter.
+	def hello_recv(self, seq_no):
+		self.ack_no = seq_no
+		self.packet.create_packet(version=self.version, flags=0, senderID=self.server.sender_id,
+			txlocalID=self.local_session_id, txremoteID=self.remote_session_id, sequence=self.seq_no,
+			ack=self.ack_no, otype='HELLO', ocode='RESPONSE')  
 		self.server.sock.sendto(self.packet.build_packet(), (self.remote_ip, self.remote_port) )
 		self.state = Connection.State.HELLO_RECVD
 		# TODO set timers
 	
 	def handle(self, packet):
-		if packet.otype == 1 and self.state == Connection.State.HELLO_SENT:
+		if packet.otype == OPERATION['HELLO'] and packet.ocode == CODE['RESPONSE'] and \
+				self.state == Connection.State.HELLO_SENT:
 			self.remote_session_id = packet.txlocalID
-			self.packet.create_packet(self.version, 0, self.server.sender_id, self.local_session_id,
-				self.remote_session_id, self.seq_no, self.ack_no, 1)  
+			self.packet.create_packet(version=self.version, flags=0, senderID=self.server.sender_id,
+				txlocalID=self.local_session_id, txremoteID=self.remote_session_id,
+				sequence=self.seq_no, ack=self.ack_no, otype='HELLO', ocode='RESPONSE')  
 			# TODO set remote sender id and ack no
 			self.server.sock.sendto(self.packet.build_packet(), (self.remote_ip, self.remote_port) )
 			self.state = Connection.State.CONNECTED
 			self.logger.info('state set to connected')
-		elif packet.otype == 1 and self.state == Connection.State.HELLO_RECVD:
+		elif packet.otype == OPERATION['HELLO'] and packet.ocode == CODE['RESPONSE'] and \
+				self.state == Connection.State.HELLO_RECVD:
 			self.state = Connection.State.CONNECTED
 			self.logger.info('state set to connected')
 		else:

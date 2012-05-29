1be00ef85ea720b95777aee82ce72e97f95a782b
Jesus <Jesus@acer>
2012-05-30 00:42:41 +0300

    new files

--- src/FileSystem.py
+++ src/FileSystem.py
@@ -11,8 +11,14 @@ class FileSystem:
     def __init__(self,path, private_dir):
         self.logger = logging.getLogger("FileSystem")
         self.root_path = path
+        if private_dir.endswith('/'):
+            private_dir = private_dir[:-1]
         self.private_dir = private_dir
+        print "private_dir: ",private_dir
         self.manifest_path = self.private_dir+'/'+'.manifest'
+        self.manifest_path_hash = self.private_dir+'/'+'.manifest_hash'
+        print "The manifest_path:", self.manifest_path
+        print "The manifest_path_hash:", self.manifest_path_hash
         self.set_private_folder()
         self.exit_flag = False
         self.thread_id = 0
@@ -21,11 +27,11 @@ class FileSystem:
         self.current_dic = {}
         self.previous_dic = {}
         self.diff_dic = {}
-        
-    def start_thread(self,):
-        self.thread_id = thread.start_new_thread(self.loop,())
-        
-    def loop(self,):
+
+    def start_thread(self, timeout=1):
+        self.thread_id = thread.start_new_thread(self.loop,(timeout,))
+
+    def loop(self,timeout):
         try:
             flag = True
             if self.exists_manifest():
@@ -36,11 +42,11 @@ class FileSystem:
                 #self.logger.warning("Manifest file not found!")
                 self.starting_dic = self.get_file_list(1)
                 self.current_dic = self.starting_dic
-                
+
             print "\nInitial content of the manifest file"
             self.print_manifest_dic(self.starting_dic)
             #self.current_dic = self.get_file_list(1)
-            
+
             while not self.exit_flag:
                 #print "\n************************************************************************"
                 '''
@@ -59,7 +65,7 @@ class FileSystem:
                 else:
                 '''
                 self.current_dic = self.get_file_list(1)
-                
+
                 self.diff_dic = self.diff_manifest(self.current_dic, self.previous_dic)
                 #print "\nPrinting diff dictionary from current to previous"
                 #self.print_manifest_dic(self.diff_dic)
@@ -74,28 +80,28 @@ class FileSystem:
                 #print self.get_local_manifest()
                 #self.print_manifest_dic(self.current_dic)
                 #print('\n')
-                    
+
                 self.write_manifest(self.current_dic)
                 #Update hash manifest
-                self.hash_manifest = FileSystem.get_md5sum_hex(self.manifest_path, block_size=2**20)
-                #print "hash_manifest", self.hash_manifest
+                self.hash_manifest = FileSystem.get_md5sum_hex(self.manifest_path_hash, block_size=2**20)
+                print "hash_manifest:", self.hash_manifest
                 #Sleep for 1 second and detect changes
-                time.sleep(5)
-                
+                time.sleep(timeout)
+
         except Exception as e:
             print "Something nasty happened!"
             print e.args
-            
+
     def terminate_thread(self):
         self.exit_flag = True
-        
+
     def set_private_folder(self):
         try:
             os.chdir(self.root_path)
         except OSError, ex:
             self.logger.error("root folder not found! %s" % (str(ex)))
             sys.exit(1)
-            
+
         if not os.access(self.private_dir, os.F_OK):
             self.logger.warning("creating private directory")
             os.mkdir(self.private_dir)
@@ -116,65 +122,69 @@ class FileSystem:
             entry = (temp[FILETYPE],temp[FILEPATH],temp[FILESIZE],temp[FILETSTAMP],temp[FILEHASH])
             tempdic[(temp[FILETYPE],temp[FILEPATH])] = entry
         return tempdic
-        
+
     def write_manifest(self, infodic):
         fd = open(self.manifest_path,'w')
+        fd2 = open(self.manifest_path+'_hash','w')
         for key,value in sorted(infodic.iteritems(), key=lambda (k,v): (v[FILETSTAMP],k), reverse=True):
             towrite = "%s\t%s\t%s\t%s\t%s" % (value[FILETYPE],value[FILEPATH],value[FILESIZE],value[FILETSTAMP],value[FILEHASH])
             fd.write(towrite+'\r\n')
+            towrite = "%s\t%s\t%s\t%s" % (value[FILETYPE],value[FILEPATH],value[FILESIZE],value[FILEHASH])
+            fd2.write(towrite+'\r\n')
             #self.logger.debug(towrite)
         fd.close()
-    
+        fd2.close()
+
     def print_manifest(self, infolist):
         for i in infolist:
             print "%s\t%s\t%s\t%s\t%s" % (i[FILETYPE],i[FILEPATH],i[FILESIZE],i[FILETSTAMP],i[FILEHASH])
-    
+
     def print_manifest_dic(self, infodic):
         if len(infodic) != 0:
             for key,value in sorted(infodic.iteritems(), key=lambda (k,v): (v[FILETSTAMP],k), reverse=True):
                 print "%s\t%s\t%s\t%s\t%s" % (value[FILETYPE],value[FILEPATH],value[FILESIZE],value[FILETSTAMP],value[FILEHASH])
-    
+
     def join_entries(self, itemlist, token):
         tmplist = []
         for item in itemlist:
             tmplist.append(string.joinfields(item,token))
         return tmplist
-    
+
     def get_sorted_list(self, infodic):
         sortedlist = []
         if len(infodic) != 0:
             for key,value in sorted(infodic.iteritems(), key=lambda (k,v): (v[FILETSTAMP],k), reverse=True):
                 sortedlist.append([value[FILETYPE],value[FILEPATH],value[FILESIZE],value[FILETSTAMP],value[FILEHASH]])
         return sortedlist
-    
+
     def get_local_manifest(self):
         current_list = self.get_sorted_list(self.previous_dic)
         return self.join_entries(current_list, '?')
-    
+
     def get_diff_manifest(self, new_manifest):
         tmpdic = {}
-        
+
         for item in new_manifest:
             tmpitem = item.split('?')
             tmpdic[(tmpitem[0],tmpitem[1])] = tmpitem
-        
+
         diffdic = self.diff_manifest(tmpdic, self.previous_dic)
         return self.join_entries(self.get_sorted_list(diffdic), '?')
 
     def get_diff_manifest_remote(self, remote_manifest):
         tmpdic = {}
-        
+
         for item in remote_manifest:
             tmpitem = item.split('?')
             tmpdic[(tmpitem[0],tmpitem[1])] = tmpitem
-        
+
         diffdic = self.diff_manifest_remote(tmpdic, self.previous_dic)
         return self.join_entries(self.get_sorted_list(diffdic), '?')
-            
+
     def diff_manifest(self, newfile, oldfile):
         if len(newfile)==0 or len(oldfile)==0:
             return {}
-        
+
         diffdic  = {}
         if newfile != oldfile:
             for newkey, newvalue in newfile.items():
@@ -188,17 +198,17 @@ class FileSystem:
                 else:
                     self.logger.debug("New file '%s'" % (newvalue[FILEPATH]))
                     diffdic[('NEW',newkey[0],newkey[1])]=newvalue
-            
+
             for oldkey, oldvalue in oldfile.items():
                 if not newfile.has_key(oldkey):
                     #self.logger.debug("Ooops file removed '%s'" % (oldvalue[FILEPATH]))
                     #print oldvalue
                     if oldkey[FILETYPE]=='DIR':
-                        #self.logger.debug("Adding to difflist a deleted DIR")
+                        self.logger.debug("Deleted DIR '%s'" % (str(oldkey[FILEPATH])))
                         tmp = ('RMD',oldvalue[FILEPATH],oldvalue[FILESIZE],str(long(time.time())),oldvalue[FILEHASH])
                         diffdic[('DEL','RMD',oldvalue[FILEPATH])]=tmp
                     elif oldkey[FILETYPE]=='FIL':
-                        #self.logger.debug("Adding to difflist a deleted FIL")
+                        self.logger.debug("Deleted FIL '%s'" % (str(oldkey[FILEPATH])))
                         tmp = ('RMF',oldvalue[FILEPATH],oldvalue[FILESIZE],str(long(time.time())),oldvalue[FILEHASH])
                         diffdic[('DEL','RMF',oldvalue[FILEPATH])]=tmp
                     elif oldkey[FILETYPE]=='RMD' or oldkey[FILETYPE]=='RMF':
@@ -210,16 +220,16 @@ class FileSystem:
             #print "The manifest is the same"
             pass
         return diffdic
-   
+
 
     def diff_manifest_remote(self, remotefile, localfile):
         #if len(remotefile)==0 or len(localfile)==0:
         #    return {}
         diffdic  = {}
-        
+
         if len(localfile)==0:
             return remotefile
-        
+
         if remotefile != localfile:
             for remotekey, remotevalue in remotefile.items():
                 if localfile.has_key(remotekey):
@@ -227,7 +237,7 @@ class FileSystem:
                     if remotevalue[FILETSTAMP]>localvalue[FILETSTAMP] and (remotevalue[0]=='RMF' or remotevalue[0]=='RMD'):
                         self.logger.debug("Update local timestamp of a deleted file '%s'" % (remotevalue[FILEPATH]))
                         localvalue[FILETSTAMP] = remotevalue[FILETSTAMP]
-                        
+
                     elif remotevalue[FILETSTAMP]>localvalue[FILETSTAMP] and remotevalue[FILEHASH]!=localvalue[FILEHASH]:
                         self.logger.debug("Updated file '%s'" % (remotevalue[FILEPATH]))
                         diffdic[('NEW',remotekey[0],remotekey[1])]=remotevalue
@@ -246,13 +256,13 @@ class FileSystem:
             self.logger.debug("The manifest is the same")
             pass
         return diffdic
-   
+
 
 
     def merge_manifest(self, currentdic, diffdic):
         if len(currentdic)==0 or len(diffdic) == 0:
             return
-        
+
         for key,value in diffdic.items():
             #print "Item ", value
             if key[0] == 'DEL':
@@ -271,7 +281,7 @@ class FileSystem:
                     #print "Still no new entry over the deleted file! Leaving in dic. ", value[1]
                     currentdic[(key[1],key[2])]=[value[0],value[1],value[2],value[3],value[4]]
                 #print "Error in merge %s : %s" % (key,value)
-            
+
     def get_file_list(self, deep=0):
         tempdic = {}
         tempdic2 = {}
@@ -279,13 +289,13 @@ class FileSystem:
         for key,value in sorted(tempdic.iteritems(), key=lambda (k,v): (v[3],k), reverse=True):
             tempdic2[key]=value
         return tempdic2
-    
+
     def inspect_folder(self, top_level, infodic, deep=0):
         for f in os.listdir(top_level):
             if top_level==self.root_path and f == self.private_dir:
                 #print "Skipping private folder"
                 continue
-            
+
             pathname = os.path.join(top_level, f)
             file_stats = os.stat(pathname)
             mode = file_stats[ST_MODE]
@@ -294,7 +304,7 @@ class FileSystem:
                 fname = pathname.split(self.root_path,1)[1][1:]
                 infodic[('DIR',fname)]=['DIR',fname,'0',timestamp,'0']
                 self.inspect_folder(pathname, infodic, deep)
-                
+
             elif S_ISREG(mode):
                 hashfile = '0'
                 filesize =  str(file_stats[ST_SIZE])
@@ -306,7 +316,7 @@ class FileSystem:
 
             else:
                 self.logger.error("Not a file or a directory: %s" % (str(pathname)))
-    
+
     @staticmethod
     def get_md5sum_hex(filename, block_size=2**20):
         fd = open(filename, 'r')
@@ -318,6 +328,6 @@ class FileSystem:
             md5.update(data)
         fd.close()
         return md5.hexdigest()
-    
+
     def get_hash_manifest(self):
         return self.hash_manifest
--- src/SyncCFT.py
+++ src/SyncCFT.py
@@ -7,7 +7,7 @@ from Security import *
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%d.%m.%y %H:%M:%S', filename='SyncCFT.log', filemode='w')
 console = logging.StreamHandler()
-console.setLevel(logging.DEBUG) 
+console.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(levelname)s: %(name)s: %(message)s')
 console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
@@ -18,23 +18,23 @@ class SyncCFT:
         self.logger = logging.getLogger("SyncCFT 1.0")
         self.logger.info("Starting SyncCFT 1.0 at %s" % (str(time.time())))
         self.exit_flag = 0
-        
+
         config = Configuration(sys.argv)
-        
+
         conf_values = config.load_configuration()
         self.local_password = config.load_password()
-        
+
         if not conf_values:
             self.logger.error("An error occurred while loading the configuration!")
             return
-        
+
         (self.port, self.folder, self.p_prob, self.q_prob, self.peers) = conf_values
-        #Logging of configuration 
+        #Logging of configuration
         self.logger.info("Listening on UDP port %s" % (str(self.port)))
         self.logger.info("Synchronizing folder %s" % (str(self.folder)))
         self.logger.info("'p' parameter: %s" % (str(self.p_prob)))
         self.logger.info("'q' parameter: %s" % (str(self.q_prob)))
-        
+
         i=1
         for item in self.peers:
             self.logger.info("Peer %d: %s:%s" % (i,str(item[0]),str(item[1])))
@@ -44,53 +44,58 @@ class SyncCFT:
         self.packetmanager = PacketManager()
         self.security = Security()
         self.fsystem = FileSystem(self.folder, '.private')
-        
+
         (self.privateKey,self.publicKey) = self.security.generate_keys(1024)
         self.publicKey_plaintext = self.security.export_key(self.publicKey)
 
-        self.fsystem.start_thread()
-        
+        self.fsystem.start_thread(timeout=1)
+
         try:
             while not self.exit_flag:
                 time.sleep(5)
         except Exception:
             pass
-        
+
+        print self.fsystem.current_dic
+        self.fsystem.terminate_thread()
+
+        '''
         print "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
         print "Here comes our new coding!!\n"
         mylist = ['FIL?f1?5?1330560662?5310dab750cabf7e2d1f307554874f9a','RMF?f3?11?1339999999?a73c45107081c08dd4560206b8ef8205']
         print "The difference of the manifests are..."
         print self.fsystem.get_diff_manifest_remote(mylist)
         print "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-        
+
         print self.fsystem.current_dic
-        
+
         self.fsystem.terminate_thread()
+
 
         pwdA= 'passwordA'
         (privateKA,publicKA) = self.security.generate_keys(1024)
         exp_publicKA = self.security.export_key(publicKA)
         hash_A = self.security.calculate_key_hash(exp_publicKA, pwdA)
-        
+
         #(privateKB,publicKB) = self.security.generate_keys(1024)
-        
+
         print "Creating the packet..."
         self.packetmanager.create_packet(2, ['SEC'], 0xabcd,0xfeea, 0xfee0, 3150765550, 286331153, "HELLO", "REQUEST", None, None)
         self.packetmanager.append_entry_to_TLVlist('SECURITY', exp_publicKA+'?'+hash_A)
         packet = self.packetmanager.build_packet()
-        
+
         #self.packetmanager.append_list_to_TLVlist('DATA', ['oneeeeee','twoooooooo','threeeeeeee', 'fourrrrrrr'])
         #self.packetmanager.append_entry_to_TLVlist('DATA', 'data_test')
         #self.packetmanager.append_entry_to_TLVlist('CONTROL', 'control_test')
         #self.packetmanager.append_entry_to_TLVlist('SECURITY', 'security_test')
         #packet = self.packetmanager.build_packet()
-        
+
         print "This is the packet dump"
         self.packetmanager.hex_packet()
-        
+
         #raw_data = '\x29\x02\xAB\xCD\xFE\xEA\xFE\xE0\xBB\xCC\xDD\xEE\x11\x11\x11\x11\x01\x01\x4C\xA9\x02\x30\x30\x30\x38\x6F\x6E\x65\x65\x65\x65\x65\x65\x02\x30\x30\x31\x30\x74\x77\x6F\x6F\x6F\x6F\x6F\x6F\x6F\x6F\xFF\x30\x30\x31\x31\x74\x68\x72\x65\x65\x65\x65\x65\x65\x65\x65'
         #raw_packet = self.packetmanager.create_packet(rawdata = raw_data)
-        
+
         print "\n\n\n"
         print self.packetmanager.get_version()
         print self.packetmanager.get_flags()
@@ -105,66 +110,67 @@ class SyncCFT:
         print self.packetmanager.get_TLVlist()
         print "This is the get_TLVlist_typevalue"
         print self.packetmanager.get_TLVlist_typevalue()
-        
+
         print "self.packetmanager.get_TLVlist('SECURITY')"
         print self.packetmanager.get_TLVlist('SECURITY')
         security_payload = self.packetmanager.get_TLVlist('SECURITY')
         recovered_plaintextkey = security_payload[0].split('?')[0]
         recovered_hash = security_payload[0].split('?')[1]
-        
+
         recovered_key = self.security.import_key(recovered_plaintextkey)
         print recovered_key
-        
+
         if self.security.calculate_key_hash(recovered_plaintextkey, pwdA) == recovered_hash:
             print "Access granted!"
-        
+
+        '''
         '''
         original_packet = packet[:]
-        
+
         print "\n\n\n*********************************************************\n\n\n"
         print "The following reprensents a communication between 2 peers"
-        
+
         password_A= 'ProtocolDesign'
         password_B= 'ProtocolDesig'
-        
+
         (privateKA,publicKA) = self.security.generate_keys(1024)
         (privateKB,publicKB) = self.security.generate_keys(1024)
-        
+
         print "Peer-A wants to send"
         self.print_hex(original_packet)
-        
+
         print "Peer-A encrypts with Public_Key_B"
         encrypted_packet = self.security.encrypt(publicKB, original_packet)
         self.print_hex(encrypted_packet)
-        
+
         print "Peer-B decrypts with Private_Key_B"
         decrypted_packet = self.security.decrypt(privateKB, encrypted_packet)
         self.print_hex(decrypted_packet)
-        
+
         if original_packet == decrypted_packet:
             print "Both packets are the same after the crypto!!!"
-        
+
         #This is the hash sent by A
         exp_publicKA = self.security.export_key(publicKA)
         hash_A = self.security.calculate_key_hash(exp_publicKA, password_A)
-        
+
         #B calculates the following
         hash_B = self.security.calculate_key_hash(exp_publicKA, password_A)
-        
+
         if hash_A == hash_B:
             print "Access granted!"
         else:
             print "Wrong password!"
         '''
-        
+
         return
-        
+
     def signal_handler(self, signal, frame):
         self.logger.warning("You pressed Ctrl+C")
         print "\nYou pressed Ctrl+C!\n"
         self.exit_flag = 1
         #sys.exit(1)
-        
+
     def print_hex(self, text):
         l = len(text)
         i = 0
@@ -189,7 +195,7 @@ class SyncCFT:
                     r=r+i2
             print r
             i += 16
-            
+
 my_app = SyncCFT()
 my_app.start_SyncCFT()
 
